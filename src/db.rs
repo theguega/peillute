e55cb898be9471dcc8fab93d515325dcd159fe3d
alexandre <alexandre.eberhardt@savoirfairelinux.com>
2025-04-16 15:49:23 +0200

    Transactions: working squeleton for sending and receiving monney

--- src/db.rs
+++ src/db.rs
@@ -4,6 +4,7 @@ use std::io;
 use std::io::{Write, BufReader, BufRead, ErrorKind};
 use std::fs::File;
 use std::cmp::Ordering;
+use rusqlite::ffi::SQLITE_NULL;
 use rusqlite::{params, Connection, Result};
 
 
@@ -11,12 +12,37 @@ fn main() -> Result<()> {
 
     
 
-    scenario1();
-
-    //drop()
+    scenario2();
+    //let name="Alice";
+    //calculate_solde(name);
+    //drop();
     //tests()
+    Ok(())
+}
+
+
+fn calculate_solde(name: &str) -> Result<()> {
+    let conn = Connection::open("database.db")?;
+
+    let mut stmt = conn.prepare(
+        "SELECT
+            IFNULL((SELECT SUM(amount) FROM Transactions WHERE to_user = ?1), 0) -
+            IFNULL((SELECT SUM(amount) FROM Transactions WHERE from_user = ?1), 0)
+
+        AS difference;"
+    )?;
+
+    let solde: f64 = stmt.query_row(params![name], |row| row.get(0))?;
+    println!("User '{}' has a calculated solde of {}€", name, solde);
 
     Ok(())
+}
+
+fn update_solde() -> Result<()> {
+
+    
+    Ok(())
+
 }
 
 fn drop() -> Result<()> {
@@ -27,21 +53,49 @@ fn drop() -> Result<()> {
     Ok(())
 }
 
+fn scenario2() -> Result<()>{
+
+    let noeud = "A"; // noeud non mutable
+    let mut local_lamport_time: &mut i64 =&mut 0; // temps de lamport local qui est incrémenté à chaque action
+
+    let conn: Connection = rusqlite::Connection::open("database.db")?;
+    drop_table(&conn);
+    init_db();
+    create_user(&conn, "Alice")?;
+    create_user(&conn, "Bob")?;
+    print_users(&conn)?;
+
+    deposit_user(&conn, "Alice", 150.0, local_lamport_time, noeud,);
+    deposit_user(&conn, "Bob", 250.0, local_lamport_time, noeud,);
+
+
+    create_tsx(&conn, "Alice", "Bob", 100.0, local_lamport_time, noeud, "Cookie");
+    create_tsx(&conn, "Bob", "Alice", 79.0, local_lamport_time, noeud, "Pizza party");
+
+    calculate_solde("Alice");
+    calculate_solde("Bob");
+
+    print_tsx(&conn)?;
+    Ok(())
+}
 fn scenario1() -> Result<()>{
 
     let noeud = "A"; // noeud non mutable
-    let mut local_lamport_time=0; // temps de lamport local qui est incrémenté à chaque action
+    let mut local_lamport_time: &mut i64 =&mut 0; // temps de lamport local qui est incrémenté à chaque action
 
     let conn: Connection = rusqlite::Connection::open("database.db")?;
+    drop_table(&conn);
     init_db();
 
-    create_user(&conn, "Alice", 153.0)?;
-    create_user(&conn, "Bob", 212.0)?;
+    create_user(&conn, "Alice")?;
+    create_user(&conn, "Bob")?;
     print_users(&conn)?;
 
-    let (from_user, to_user, amount, lamport_time, source_node, optionnal_msg)=("Alice", "Bob", 100.0, 0, "A", "Cadeau");
+    let (from_user, to_user, amount, source_node, optionnal_msg)=("Alice", "Bob", 100.0, "A", "Cadeau");
 
-    create_tsx(&conn, from_user, to_user, amount, lamport_time, source_node, optionnal_msg);
+    create_tsx(&conn, from_user, to_user, amount, local_lamport_time, source_node, optionnal_msg);
+    create_tsx(&conn, from_user, to_user, amount, local_lamport_time, source_node, optionnal_msg);
+    create_tsx(&conn, "Bob", "Alice", 79.0, local_lamport_time, source_node, optionnal_msg);
 
     print_tsx(&conn)?;
     Ok(())
@@ -57,7 +111,7 @@ fn tests()  -> Result<()>{
     let res = init_db();
     println!("{}","ok");
     // création d'un user
-    create_user(&conn, "Alice", 150.0)?;
+    create_user(&conn, "Alice")?;
     //println!("Users : ");
     print_users(&conn)?;
     //println!("Transactions : ");
@@ -76,24 +130,40 @@ fn drop_table(conn: &rusqlite::Connection) -> rusqlite::Result<()> {
 }
 
 
-fn create_user(conn: &rusqlite::Connection, unique_name: &str, solde: f64) -> rusqlite::Result<()> {
+fn create_user(conn: &rusqlite::Connection, unique_name: &str) -> rusqlite::Result<()> {
+    conn.execute(
+        "INSERT INTO User (unique_name, solde) VALUES (?1,0)",
+        rusqlite::params![unique_name],
+    )?;
+    println!("User '{}' added with solde 0", unique_name);
+    Ok(())
+}
+
+fn create_user_solde(conn: &rusqlite::Connection, unique_name: &str, solde: f64, lamport_time: &mut i64, source_node: &str ) -> rusqlite::Result<()> {
     conn.execute(
         "INSERT INTO User (unique_name, solde) VALUES (?1, ?2)",
         rusqlite::params![unique_name, solde],
     )?;
+
+    create_tsx(&conn, "", unique_name, solde, lamport_time, source_node, "Deposit");
+
     println!("User '{}' added with solde {}", unique_name, solde);
     Ok(())
 }
 
-fn deposit_user(conn: &rusqlite::Connection, unique_name: &str, amount: f64) -> rusqlite::Result<()> {
+
+fn deposit_user(conn: &rusqlite::Connection, unique_name: &str, amount: f64, lamport_time: &mut i64, source_node: &str) -> rusqlite::Result<()> {
     if amount<0.0{
         // amount should be >0
         return Err(rusqlite::Error::InvalidQuery);
     }
+    create_tsx(&conn, "NULL", unique_name, amount, lamport_time, source_node, "Deposit");
+
     conn.execute(
         "UPDATE User SET solde = solde + ? WHERE unique_name = ?",
         rusqlite::params![amount, unique_name],
     )?;
+
     
     println!("User '{}' deposed {}€ in User", unique_name, amount);
 
@@ -101,13 +171,13 @@ fn deposit_user(conn: &rusqlite::Connection, unique_name: &str, amount: f64) -> 
 }
 
 
-fn create_tsx(conn: &rusqlite::Connection, from_user: &str, to_user: &str, amount: f64, lamport_time: i64, source_node: &str, optionnal_msg: &str) -> rusqlite::Result<()> {
+fn create_tsx(conn: &rusqlite::Connection, from_user: &str, to_user: &str, amount: f64, lamport_time: &mut i64, source_node: &str, optionnal_msg: &str) -> rusqlite::Result<()> {
     conn.execute(
         "INSERT INTO Transactions (from_user, to_user, amount, lamport_time, source_node, optionnal_msg) VALUES (?1, ?2, ?3, ?4, ?5, ?6)",
-        rusqlite::params![from_user, to_user, amount, lamport_time, source_node, optionnal_msg],
+        rusqlite::params![from_user, to_user, amount, *lamport_time, source_node, optionnal_msg],
     )?;
     println!("from_user: {}, to_user: {}, amount: {}, lamport_time: {}, source_node: {}, optionnal_msg: {}", from_user, to_user, amount, lamport_time, source_node, optionnal_msg);
-
+    *lamport_time+=1;
     Ok(())
 }
 
@@ -180,7 +250,7 @@ fn init_db() -> Result<()> {
     // Attention : le mot Transaction sans S est reservé dans SQL donc on en peut pas l'utiliser (comme Select par exemple)
     conn.execute(
         "CREATE TABLE IF NOT EXISTS Transactions (
-            from_user TEXT NOT NULL,
+            from_user TEXT,
             to_user TEXT NOT NULL,
             amount FLOAT NOT NULL,
             lamport_time INTEGER NOT NULL,
